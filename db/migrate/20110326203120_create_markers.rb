c47963d4a85f379f691c8f512a793dc234c77245
Ben Quick <github@benquick.net>
2011-03-26 20:57:43 +0000

    Page 52

---
+++ db/migrate/20110326203120_create_markers.rb
@@ -0,0 +1,17 @@
+class CreateMarkers < ActiveRecord::Migration
+  def self.up
+    create_table :markers do |t|
+
+      t.column :lat, :decimal
+      t.column :lng, :decimal
+      t.column :found, :string, :limit=>100
+      t.column :left, :string, :limit=>100
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :markers
+  end
+end
